1d24f852a3eba6b724b42bcbb960ef4bb8f54e02
Khr0n0ss <jenfantbis@laposte.net>
2026-02-11 10:53:29 +0100

    CLickable

--- Assets/Script/S_Clickable.cs
+++ Assets/Script/S_Clickable.cs
@@ -3,22 +3,25 @@ using UnityEngine;
 public class Clickable : MonoBehaviour
 {
     public ScriptableObject self;
+    public S_TurnBase _interaction;
 
     void Start()
     {
-        
+        _interaction = FindAnyObjectByType<S_TurnBase>();
     }
 
     //Agrandie lorsque souris dessus
     void OnMouseEnter()
     {
-        transform.localScale = transform.localScale * 1.1f;
+        if(_interaction._nextButton.interactable)
+            transform.localScale = transform.localScale * 1.1f;
     }
 
     //Rétrécie lorsque souris dessus
     void OnMouseExit()
     {
-        transform.localScale = transform.localScale / 1.1f;
+        if(_interaction._nextButton.interactable)
+            transform.localScale = transform.localScale / 1.1f;
     }
 
 }
