43ca7a500714bb9438cd0258a024c415fb0c9f88
Khr0n0ss <jenfantbis@laposte.net>
2026-02-11 10:25:49 +0100

    Click

--- Assets/Script/S_Clickable.cs
+++ Assets/Script/S_Clickable.cs
@@ -2,24 +2,16 @@
 using UnityEngine;
 public class Clickable : MonoBehaviour
 {
-
-
-    public GameObject self;
-    void Update()
-    {
-        
-    }
-
     //Agrandie lorsque souris dessus
     void OnMouseEnter()
     {
-        self.transform.localScale = self.transform.localScale * 1.1f;
+        transform.localScale = transform.localScale * 1.1f;
     }
 
     //Rétrécie lorsque souris dessus
     void OnMouseExit()
     {
-        self.transform.localScale = self.transform.localScale / 1.1f;
+        transform.localScale = transform.localScale / 1.1f;
     }
 
 }
