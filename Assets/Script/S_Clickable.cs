3f09e0a348a5fd195c168f2095760415f1fd477b
Normandu75 <dufavet.norman@outlook.fr>
2026-02-11 11:21:00 +0100

    Les stats

--- Assets/Script/S_Clickable.cs
+++ Assets/Script/S_Clickable.cs
@@ -1,12 +1,27 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 public class Clickable : MonoBehaviour
 {
-    public ScriptableObject self;
+    public Membre_Stat self;
+    private string name; 
+    private string description;
+    private int dégâts;
+    private int PVMax;
+    private Sprite sprite;
+
 
     void Start()
     {
+        name = self.MembreName;
+
+        description = self.Description;
+
+        dégâts = self.dégâts;
+
+        PVMax = self.PVMax;
         
+        sprite = self.sprite;
     }
 
     //Agrandie lorsque souris dessus
