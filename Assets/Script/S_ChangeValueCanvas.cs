de21d55385586fda6223183311604f29d04073ab
Khr0n0ss <jenfantbis@laposte.net>
2026-02-11 11:46:12 +0100

    ChangeValue

--- Assets/Script/S_ChangeValueCanvas.cs
+++ Assets/Script/S_ChangeValueCanvas.cs
@@ -6,4 +6,17 @@ using TMPro;
 public class S_ChangeValueCanvas : MonoBehaviour
 {
     public List<TMP_Text> _txtMembre = new List<TMP_Text>();
+
+    void Start()
+    {
+        changeText();
+    }
+
+    public void changeText()
+    {
+        foreach(TMP_Text _text in _txtMembre)
+        {
+            _text.text += " " + "3 / 3" ;
+        }
+    }
 }
